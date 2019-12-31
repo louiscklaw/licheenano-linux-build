79cbb8efffe0045a4d0518c0404f017a53e590dc
Travis CI <travis@travis-ci.org>
2019-12-31 18:21:40 +0800

    init commit and test,

--- docker-share/rootfs/build.sh
+++ docker-share/rootfs/build.sh
@@ -4,17 +4,20 @@ echo 'building rootfs'
 
 cd docker-share/rootfs
 
-rm -rf buildroot-*
+  rm -rf buildroot-*
 
-wget https://buildroot.org/downloads/buildroot-2017.08.tar.gz
-tar xvf buildroot-2017.08.tar.gz
+  wget https://buildroot.org/downloads/buildroot-2017.08.tar.gz
+  tar xvf buildroot-2017.08.tar.gz
 
-cp .config buildroot-2017.08/.config
+  cp .config buildroot-2017.08/.config
 
-cd buildroot-2017.08/
-make -j6
-cd ..
+  cd buildroot-2017.08/
+    make -j6
+  cd ..
 
 cd ../..
+
+ls -l docker-share/rootfs/buildroot-2017.08/output/images
+ls -l docker-share/rootfs/buildroot-2017.08/output/images/rootfs.tar
 
 echo 'build rootfs done.'
